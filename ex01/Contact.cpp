5262bdc4db61e159ac87ad262cffc9db4f6d8436
Yosshii-42 <yoshi.tsurudome@gmail.com>
2025-02-06 14:14:08 +0900

    Contact modefied

--- ex01/Contact.cpp
+++ ex01/Contact.cpp
@@ -3,29 +3,29 @@
 void    Contact::setContact(const int idx, const std::string& f_name, 
                             const std::string& l_name, const std::string& n_name,
                             const std::string& phone, const std::string& secret) {
-    index = idx;
-    first_name = f_name;
-    last_name = l_name;
-    nickname = n_name;
-    phone_number = phone;
-    darkest_secret = secret;
+    _index = idx;
+    _first_name = f_name;
+    _last_name = l_name;
+    _nickname = n_name;
+    _phone_number = phone;
+    _darkest_secret = secret;
 }
 
 void    Contact::displayShortContact(void) const {
-    std::cout << std::right << std::setw(10) << index << "|";
-    printStr(first_name, BAR);
-    printStr(last_name, BAR);
-    printStr(nickname, NL);
+    std::cout << std::right << std::setw(10) << _index << "|";
+    printStr(_first_name, BAR);
+    printStr(_last_name, BAR);
+    printStr(_nickname, NL);
 }
 
 void    Contact::displayFullContact(void) const {
     std::cout << "-----------------------------------" << std::endl;
-    std::cout << "first name     : " << first_name << std::endl;
-    std::cout << "last name      : " << last_name << std::endl;
-    std::cout << "nickname       : " << nickname << std::endl;
-    std::cout << "phone number   : " << phone_number << std::endl;
+    std::cout << "first name     : " << _first_name << std::endl;
+    std::cout << "last name      : " << _last_name << std::endl;
+    std::cout << "nickname       : " << _nickname << std::endl;
+    std::cout << "phone number   : " << _phone_number << std::endl;
     std::cout << "darkest secret : ";
-    for (int i(0); i < (int)darkest_secret.length(); ++i)
+    for (int i(0); i < (int)_darkest_secret.length(); ++i)
         std::cout << ".";
     std::cout << std::endl;
     std::cout << "-----------------------------------" << std::endl;
@@ -41,5 +41,5 @@ void    Contact::printStr(const std::string& str, int end) const {
 }
 
 bool    Contact::isRegistered(void) const {
-    return (!first_name.empty());
+    return (!_first_name.empty());
 }
--- ex01/Contact.hpp
+++ ex01/Contact.hpp
@@ -10,12 +10,12 @@
 
 class Contact {
 private:
-    int     index;
-    std::string first_name;
-    std::string last_name;
-    std::string nickname;
-    std::string phone_number;
-    std::string darkest_secret;
+    int         _index;
+    std::string _first_name;
+    std::string _last_name;
+    std::string _nickname;
+    std::string _phone_number;
+    std::string _darkest_secret;
 
 public:
     void    setContact(const int idx, const std::string& f_name,
